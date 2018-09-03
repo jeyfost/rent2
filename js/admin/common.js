b7ba3c6f60d4e2de73439d20fe042888d479dac3
jeyfost <foster_andrew@tut.by>
2018-09-03 11:12:19 +0300

    Создана главная страница панели администрирования

--- js/admin/common.js
+++ js/admin/common.js
@@ -1,7 +1,41 @@
+$(window).on("load", function () {
+    if($('*').is($('#leftMenu'))) {
+        if($('#leftMenu').height() < parseInt($(window).height() - $('#topLine').height())) {
+            $('#leftMenu').height(parseInt($(window).height() - $('#topLine').height()));
+        }
+    }
+
+    if($('*').is($('#content'))) {
+        $('#content').width(parseInt($(window).width() - $('#leftMenu').width() - 130));
+    }
+});
+
+$(window).on("resize", function () {
+    if($('*').is($('#leftMenu'))) {
+        if($('#leftMenu').height() < parseInt($(window).height() - $('#topLine').height())) {
+            $('#leftMenu').height(parseInt($(window).height() - $('#topLine').height()));
+        }
+    }
+
+    if($('*').is($('#content'))) {
+        $('#content').width(parseInt($(window).width() - $('#leftMenu').width() - 130));
+    }
+});
+
 function buttonHover(id, action) {
     if(action === 1) {
         document.getElementById(id).style.backgroundColor = "#ffdb54";
     } else {
         document.getElementById(id).style.backgroundColor = "#dedede";
     }
+}
+
+function exit() {
+    $.ajax({
+        type: "POST",
+        url: "/scripts/admin/ajaxExit.php",
+        success: function () {
+            window.location.href = "../";
+        }
+    });
 }
---
+++ scripts/admin/ajaxExit.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeyfost
+ * Date: 03.09.2018
+ * Time: 11:06
+ */
+
+session_start();
+unset($_SESSION['userID']);
